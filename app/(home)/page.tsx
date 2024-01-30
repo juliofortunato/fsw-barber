9908831ad51b455067ef7cacfab4974e3f4f5dab
Júlio Fortunato <juliofortunato58@gmail.com>
2024-01-30 11:16:42 -0300

    ✨ feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { format } from "date-fns";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM 'de' yyyy", { locale: ptBR })}</p>
+      </div>
     </div>
   );
 }
