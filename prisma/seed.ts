9f6cc8b26f35ba08dddf703032f3a315fc61c7e7
Júlio Fortunato <juliofortunato58@gmail.com>
2024-01-31 15:39:45 -0300

    🐞 fix: seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -95,7 +95,8 @@ async function seedDatabase() {
         name: "Hidratação",
         description: "Hidratação profunda para cabelo e barba.",
         price: 25.0,
-        imageUrl: "Fios hidratados, macios e brilhantes.",
+        imageUrl:
+          "https://images.unsplash.com/photo-1598524374668-5d565a3c42e8?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
     ];
 
